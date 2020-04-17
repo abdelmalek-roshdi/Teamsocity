a66a5c47b2e298a0e2a4647a147a48c8c7cbfa64
abd-elmalek <malekroshdi@gmail.com>
2020-04-17 06:15:15 -0700

    Initial Commit

---
+++ Teamsocity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teamsocity
+//
+//  Created by Abd-Elmalek on 4/17/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
