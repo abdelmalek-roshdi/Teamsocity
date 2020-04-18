53277640dbbdbd79b08fdee324afe0a95ea47751
abd-elmalek <malekroshdi@gmail.com>
2020-04-18 16:49:00 -0700

    main view implemented

---
+++ Teamsocity/common/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Teamsocity
+//
+//  Created by Abd-Elmalek on 4/18/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let BASE_URL_MAIN = "https://www.thesportsdb.com/api/v1/json/1/all_sports.php"
+    static let SPORTS = "sports"
+    static let idSport = "idSport"
+    static let strSport = "strSport"
+    static let strFormat = "strFormat"
+    static let strSportThumb = "strSportThumb"
+    static let strSportThumbGreen = "strSportThumbGreen"
+    static let strSportDescription = "strSportDescription"
+    static let sportsArrayNotification = "sportsArrayNotification"
+}
