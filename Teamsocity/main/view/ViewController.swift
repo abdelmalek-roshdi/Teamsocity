5e1a1acb1d10bcc2c6a2dc4d392f950daab070b3
abd-elmalek <malekroshdi@gmail.com>
2020-04-19 07:03:30 -0700

    UI Enhancements

--- Teamsocity/main/view/ViewController.swift
+++ Teamsocity/main/view/ViewController.swift
@@ -23,8 +23,12 @@ class ViewController: UIViewController,MainViewProtocol {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         mainPressenter = MainPresenter(mainView: self)
         mainPressenter?.loadSports()
+        
+        
         
     }
     
@@ -54,46 +58,63 @@ extension ViewController : UICollectionViewDelegate, UICollectionViewDataSource,
         return cell
     }
 
-   
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//        return UIEdgeInsets.zero
-//    }
 //
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-//
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
      func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
            print("selected item at row \(indexPath.row) and section \(indexPath.section)")
            print("selected item \(indexPath.item)")
        }
     
-     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-           let cellWidth = UIScreen.main.bounds.width / 3 - 5
-           let cellHight = UIScreen.main.bounds.height / 3 - 5
-         return CGSize(width: cellWidth, height: cellHight)
-    }
+//     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let cellWidth = UIScreen.main.bounds.width / 3.0
+//        let cellHight = UIScreen.main.bounds.height / 3.0
+//         return CGSize(width: cellWidth, height: cellHight)
+//    }
+//
     
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+//        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//
+//
+//
+//       let cellWidth = UIScreen.main.bounds.width / 3.0
+//       let cellHight = UIScreen.main.bounds.height / 3.0
+//
+//        return CGSize(width: width, height: height);
+//    }
     
-    func setupCollectionViewItemSize(){
-        if collectionViewFlowLayout == nil{
-            let itemsPerRow:CGFloat = 2
-            let lineSpacing:CGFloat = 5
-            let interItemSpacing:CGFloat = 5
-            
-            let width = (collectionView.frame.width - (itemsPerRow - 1) * interItemSpacing) / itemsPerRow
-            let height = width
-            collectionViewFlowLayout = UICollectionViewFlowLayout()
-            collectionViewFlowLayout?.itemSize = CGSize(width: width, height: height)
-            collectionViewFlowLayout?.sectionInset = UIEdgeInsets(top: lineSpacing, left: lineSpacing, bottom: 0, right: lineSpacing)
-         
-            collectionView.setCollectionViewLayout(collectionViewFlowLayout!, animated: true)
-        }
-    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = UIScreen.main.bounds.width / 2.0
+        let cellHight = UIScreen.main.bounds.height / 5.0
+           return CGSize(width: cellWidth, height: cellHight)
+       }
+    
+//    func setupCollectionViewItemSize(){
+//        if collectionViewFlowLayout == nil{
+//            let itemsPerRow:CGFloat = 2
+//            let lineSpacing:CGFloat = 5
+//            let interItemSpacing:CGFloat = 5
+//            
+//            let width = (collectionView.frame.width - (itemsPerRow - 1) * interItemSpacing) / itemsPerRow
+//            let height = width
+//            collectionViewFlowLayout = UICollectionViewFlowLayout()
+//            collectionViewFlowLayout?.itemSize = CGSize(width: width, height: height)
+//            collectionViewFlowLayout?.sectionInset = UIEdgeInsets(top: lineSpacing, left: lineSpacing, bottom: 0, right: lineSpacing)
+//         
+//            collectionView.setCollectionViewLayout(collectionViewFlowLayout!, animated: true)
+//        }
+//    }
 
     
 }
